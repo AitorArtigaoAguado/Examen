41f02ce044903ae99658ffa02767951735713802
Aitor <aitor.artigao1995@gmail.com>
2021-02-16 19:22:54 +0100

    Navbar
    Layout para la pagina

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<div class="collapse navbar-collapse">
+		<a href="/" class="navbar-brand">Fixed navbar</a>
+		<div class="navbar-nav">
+			<a href="/" class="nav-item nav-link">Home</a> 
+			<a href="/link" class="nav-item nav-link">Link</a> 
+			<a href="/disabled" class="nav-item nav-link">Disabled</a>
+		</div>
+	</div>
+</nav>
