c91b431783848cad29b66add97e986a0b7a4946c
Aitor <aitor.artigao1995@gmail.com>
2021-02-16 19:31:15 +0100

    Rutas ordenadas
    Las rutas han sido ordenadas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | Web Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register web routes for your application. These
+ * | routes are loaded by the RouteServiceProvider within a group which
+ * | contains the "web" middleware group. Now create something great!
+ * |
+ */
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::get('/migraciones', function () {
+    return view('migraciones');
+});
+
+Route::get('/seeders', function () {
+    return view('seeders');
+});
+
+Route::get('/email', function () {
+    return view('email');
 });
